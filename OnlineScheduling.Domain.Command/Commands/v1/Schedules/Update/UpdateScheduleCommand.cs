f75f3b3865c8e364d24b9ad037d3273d041b6fb7
Fca Marília Mendonça <marilianobre1@gmail.com>
2024-01-18 16:51:36 -0300

    Teste usuário no commit

--- OnlineScheduling.Domain.Command/Commands/v1/Schedules/Update/UpdateScheduleCommand.cs
+++ OnlineScheduling.Domain.Command/Commands/v1/Schedules/Update/UpdateScheduleCommand.cs
@@ -5,7 +5,7 @@ namespace OnlineScheduling.Domain.Command.Commands.v1.Schedules.Update;
 public sealed class UpdateScheduleCommand : IRequest<Unit>
 {
     public int Id { get; set; }
-    public DateTime ScheduleAt { get; set; }
+    public DateTime? ScheduleAt { get; set; }
     public int ServiceId { get; set; }
     public int CustomerId { get; set; }
 }
